20cefc95eb11dec8c8d4ce7940cf075f9cf42d67
mdebeljuh <mdebeljuh@gmail.com>
2020-03-06 19:17:11 +0100

    Added actuator, configuration, logging

--- src/main/java/hr/vsite/njp/helloworld/HelloWorldController.java
+++ src/main/java/hr/vsite/njp/helloworld/HelloWorldController.java
@@ -1,14 +1,20 @@
 package hr.vsite.njp.helloworld;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloWorldController {
+    private static final Logger LOGGER = LoggerFactory.getLogger(HelloWorldController.class);
 
     @GetMapping("/helloworld/{name}")
     public HelloWorld helloWorld(@PathVariable String name) {
-        return new HelloWorld(1L, "Hello " + name);
+        LOGGER.trace("hello world start - {}", name);
+        HelloWorld helloWorld = new HelloWorld(1L, "Hello " + name);
+        LOGGER.trace("hello world end - {}", helloWorld);
+        return helloWorld;
     }
 }
